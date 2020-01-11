4858358617fe7992c5e9995b6f5844bdd5dd31a5
yoshi-2015 <yoshihisa.ohkura@icloud.com>
2020-01-11 08:56:13 +0000

    [Update]comment valide

--- app/controllers/memo_comments_controller.rb
+++ app/controllers/memo_comments_controller.rb
@@ -1,20 +1,22 @@
 class MemoCommentsController < ApplicationController
   def create
-    memo = Memo.find(params[:memo_id])
-    comment = current_user.memo_comments.new(memo_comment_params)
-    comment.memo_id = memo.id
-    comment.save
-    redirect_back(fallback_location: root_path)
+    @memo = Memo.find(params[:memo_id])
+    @memo_comment = current_user.memo_comments.new(memo_comment_params)
+    @memo_comment.memo_id = @memo.id
+
+    if @memo_comment.save
+      redirect_to memo_path(@memo)
+    else
+      @user = @memo.user
+      @newmemo = Memo.new
+      render template: "memos/show"
+    end
   end
 
   def destroy
     # ページからコントローラーにコメントのIDを持ってくる・・・
     # /memos/#{@memo.id}/memo_comments/#{@memo_comment.id}/を作り上げる＝memo_comment.idを探して、デストロイさせる！！
-    # memo = Memo.find(params[:memo_id])
-    # comment = current_user.memo_comments.find(params[:id])
     comment = MemoComment.find(params[:id])
-    # memo = Memo.find(params[:memo_id])
-    # comment = current_user.memo_comments.find_by(comment: comment)
     comment.destroy
     redirect_back(fallback_location: root_path)
   end
--- app/models/memo_comment.rb
+++ app/models/memo_comment.rb
@@ -1,4 +1,5 @@
 class MemoComment < ApplicationRecord
   belongs_to :user
   belongs_to :memo
+  validates :comment, presence: true, length: { maximum: 200 }
 end
